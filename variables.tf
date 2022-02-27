240a2baeef525b81a9154dfe0347381512856a77
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-02-27 19:19:07 +0800

    Switched variables places

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "log_analytics_workspace_id" {
-  description = "The log analytics workspace ID that the VMs will use"
-  type = string
-}
-
-variable "log_analytics_workspace_key" {
-  description = "The log analytics workspace key that the VMs will use"
-  type = string
-}
-
 variable "scope_type" {
   description = "The scope type of the VMs that will be modified"
   type = string
@@ -21,6 +11,16 @@ variable "scope" {
 variable "has_log_analytics_workspace" {
   description = "If Log Analytics Workspace should be installed for both Windows and Linux VMs"
   type = bool
+}
+
+variable "log_analytics_workspace_id" {
+  description = "The log analytics workspace ID that the VMs will use"
+  type = string
+}
+
+variable "log_analytics_workspace_key" {
+  description = "The log analytics workspace key that the VMs will use"
+  type = string
 }
 
 variable "has_azure_monitor" {
