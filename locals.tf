639c2020bdafcb3175b41caa3b94ba86fa39da65
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-03-03 19:33:28 +0800

    Added conditions

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  has_valid_log_analytics_workspace_values = var.has_log_analytics_workspace && var.log_analytics_workspace_id != "" && var.log_analytics_workspace_key != ""
-  has_valid_azure_monitor_values = var.has_azure_monitor
+  has_valid_log_analytics_workspace_values = var.has_log_analytics_workspace && var.subscription != "" && var.log_analytics_workspace_id != "" && var.log_analytics_workspace_key != ""
+  has_valid_azure_monitor_values = var.has_azure_monitor && var.subscription != ""
 }
